26578a319ac9ee46719f5540c37dd21763b5e789
angelalonso <alonsofonseca.angel@gmail.com>
2017-05-16 10:18:10 +0200

    first confirmation

--- scripts/listener.py
+++ scripts/listener.py
@@ -2,14 +2,26 @@
 
 import alsaaudio
 import audioop
+import pyglet
 import time
 
 THRESHOLD = 600
+CHECKTIME = .01
+
+
+def confirmation():
+    # http://stackoverflow.com/questions/307305/play-a-sound-with-python
+    # also sample from http://www.noiseaddicts.com/free-samples-mp3/?id=3739
+    music = pyglet.resource.media('../audio/noiseaddicts.com_samples_1w72b820_3739.mp3')
+    music.play()
+
+    pyglet.app.run()
+
 
 input = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK)
 
 input.setchannels(1)
-input.setrate(8000)
+input.setrate(16000)
 input.setformat(alsaaudio.PCM_FORMAT_S16_LE)
 
 input.setperiodsize(160)
@@ -22,5 +34,8 @@ while True:
         sound = audioop.max(data, 2)
         if sound > THRESHOLD:
             print "Sound is going on"
-    time.sleep(.01)
+            confirmation()
+        else:
+            print "-"
 
+    time.sleep(CHECKTIME)
