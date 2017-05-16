8d28dc8c4c58db79ccf8c4ce2b8591c68a83f270
angelalonso <alonsofonseca.angel@gmail.com>
2017-05-16 14:53:05 +0200

    script calling STT

---
+++ scripts/rec_n_check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+HW="plughw:1,0"
+DUR="3"
+
+KEY=$(cat .credentials)
+SNDFILE="test.flac"
+
+arecord -D $HW -t wav -d $DUR -r 16000 | flac - -f --best --sample-rate 16000 -o $SNDFILE
+
+curl -X POST --data-binary @$SNDFILE --user-agent 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.77 Safari/535.7' --header 'Content-Type: audio/x-flac; rate=16000;' 'https://www.google.com/speech-api/v2/recognize?output=json&lang=en-us&key='$KEY
+
