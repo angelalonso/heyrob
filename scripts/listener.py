fdc8425ef058bbdf5fd2ce47b3b9c9cfabdd64ac
angelalonso <alonsofonseca.angel@gmail.com>
2017-05-16 10:34:20 +0200

    adding my own sounds

--- scripts/listener.py
+++ scripts/listener.py
@@ -5,17 +5,32 @@ import audioop
 import pyglet
 import time
 
+
+from wave import open as waveOpen
+from ossaudiodev import open as ossOpen
+
 THRESHOLD = 600
 CHECKTIME = .01
 
 
 def confirmation():
-    # http://stackoverflow.com/questions/307305/play-a-sound-with-python
     # also sample from http://www.noiseaddicts.com/free-samples-mp3/?id=3739
-    music = pyglet.resource.media('../audio/noiseaddicts.com_samples_1w72b820_3739.mp3')
-    music.play()
-
-    pyglet.app.run()
+    s = waveOpen('listening.wav','rb')
+    (nc,sw,fr,nf,comptype, compname) = s.getparams( )
+    dsp = ossOpen('/dev/dsp','w')
+    try:
+      from ossaudiodev import AFMT_S16_NE
+    except ImportError:
+      from sys import byteorder
+      if byteorder == "little":
+        AFMT_S16_NE = ossaudiodev.AFMT_S16_LE
+      else:
+        AFMT_S16_NE = ossaudiodev.AFMT_S16_BE
+    dsp.setparameters(AFMT_S16_NE, nc, fr)
+    data = s.readframes(nf)
+    s.close()
+    dsp.write(data)
+    dsp.close()
 
 
 input = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK)
