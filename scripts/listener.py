ec0a5b144b493b25b55ffdbb4b9be939a7527850
angelalonso <alonsofonseca.angel@gmail.com>
2017-05-20 11:49:30 +0200

    testing listener loop

--- scripts/listener.py
+++ scripts/listener.py
@@ -61,7 +61,9 @@ def speechtotext():
     cmd_curl = "curl -X POST --data-binary @/dev/shm/out.flac --user-agent 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.77 Safari/535.7' --header 'Content-Type: audio/x-flac; rate=16000;' 'https://www.google.com/speech-api/v2/recognize?output=json&lang=en-us&key='"
 
     subprocess.Popen([cmd_transform], shell=True).communicate()
+    time.sleep(1)
     subprocess.Popen([cmd_curl], shell=True).communicate()
+    time.sleep(10)
 
 def original():
 
