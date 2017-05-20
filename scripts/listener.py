0e38d4551a7bfd8f4953b15a72c5fa260a812e66
angelalonso <alonsofonseca.angel@gmail.com>
2017-05-20 11:44:55 +0200

    testing listener loop

--- scripts/listener.py
+++ scripts/listener.py
@@ -24,7 +24,8 @@ def wake_confirm():
 
 def standby_listen():
     '''Constantly listens for higher volumes on the input'''
-    standby_input = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK)
+    standby_input = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,
+                                  alsaaudio.PCM_NONBLOCK)
     standby_input.setchannels(CHANNELS)
     standby_input.setrate(AUDIORATE)
     standby_input.setformat(alsaaudio.PCM_FORMAT_S16_LE)
@@ -53,6 +54,14 @@ def keyword_listen():
 def confirmation():
     '''Plays confirmation that a sound has been identified'''
     call(["play", "../audio/listening.wav"])
+
+
+def speechtotext():
+    cmd_transform = "flac " + KEYWORDFILE + " -f --best --sample-rate 16000 -o test.flac"
+    cmd_curl = "curl -X POST --data-binary @/dev/shm/out.flac --user-agent 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.77 Safari/535.7' --header 'Content-Type: audio/x-flac; rate=16000;' 'https://www.google.com/speech-api/v2/recognize?output=json&lang=en-us&key='"
+
+    subprocess.Popen([cmd_transform], shell=True).communicate()
+    subprocess.Popen([cmd_curl], shell=True).communicate()
 
 def original():
 
@@ -85,5 +94,5 @@ while True:
         trig2 = keyword_listen()
         if trig2:
             confirmation()
+            speechtotext()
             print "HEY 2"
-
