883bd0b0aa35b1dbff6a0a08626777db55474f97
aaf <aaf@zen>
2017-07-02 08:52:00 +0200

    main.py as the source of all whatevers

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+'''
+- heyrob.py as an entry point, accepting "test"
+- listen.py as the listener, both local and on web
+- understand.py as the interpreter from the audio into a decision
+- takeaction.py as the one who triggers the actions possible
+
+all of them should be interchangeable
+  (e.g.: if I want to test a better listener,
+   I should be able to just point to that one)
+
+all of them should use logging
+'''
+
+
+def main():
+    print("main")
+
+
+if __name__ == "__main__":
+    main()
+    print("done")
